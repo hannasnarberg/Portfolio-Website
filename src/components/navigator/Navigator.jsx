45159591c6c5434c231e04ffb04abd4cfd99ac94
Hanna Snarberg <hannasnaris@gmail.com>
2023-01-11 16:02:51 +0100

    Nav soon done

--- src/components/navigator/Navigator.jsx
+++ src/components/navigator/Navigator.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./navigator.css"
+import {AiOutlineHome, AiOutlineUser, AiOutlineMail} from "react-icons/ai"
+import {BsListCheck} from "react-icons/bs"
 
 const Navigator = () => {
   return (
-    <div>Navigator</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsListCheck/></a>
+      <a href="#contact"><AiOutlineMail/></a>
+    </nav>
   )
 }
 
