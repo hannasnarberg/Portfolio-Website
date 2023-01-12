ed5d16d13d0efef95fddf3734c4d928f1f258c38
Hanna Snarberg <hannasnaris@gmail.com>
2023-01-12 16:25:57 +0100

    Getting started experience page

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./experience.css"
 
+
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>
+        My skills
+      </h5>
+      <h2>My experience</h2>
+      <div className="container experience__container">
+      <div className="container experience__frontend">
+        <h3>Frontend development</h3>
+        <div className="experience__content">
+          <article className='experience__details'>
+
+          </article>
+        </div>
+      </div>
+      <div className="container experience__UX"> </div>
+      </div> 
+    </section>
   )
 }
 
